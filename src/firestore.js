6c1826abe439d3e7dea279f997513ea859faebd9
Michał Czapliński <czaplinskim@icloud.com>
2023-02-25 19:19:52 +0100

    Informacje o firestore

--- src/firestore.js
+++ src/firestore.js
@@ -7,12 +7,12 @@ import "firebase/auth";
 
 
 const config = {
-    apiKey: "AIzaSyC0EQa3VtIfpwBziPiygcSv-vu4Rv095PQ",
-    authDomain: "przejazdy-mielno.firebaseapp.com",
-    projectId: "przejazdy-mielno",
-    storageBucket: "przejazdy-mielno.appspot.com",
-    messagingSenderId: "382164111281",
-    appId: "1:382164111281:web:26f4312121985ff4e5f790"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 
